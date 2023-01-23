e0d1d6b597531f5b3116c4e7e65609c33a1a85cb
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-01-23 19:33:41 -0300

    Adicionando exercicios

---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_01_javascript_primeiros_passos/exercicio04.js
@@ -0,0 +1,10 @@
+const numero = -1;
+
+if (numero > 0){
+    console.log("Número positivo!")
+
+}else if (numero < 0){
+    console.log("Número negativo")
+}else {
+    console.log("Número Zero")
+}
