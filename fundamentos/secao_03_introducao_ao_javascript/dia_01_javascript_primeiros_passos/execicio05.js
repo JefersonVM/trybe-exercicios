7a822c3643770603aa20b3e47302f97f72325e76
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-01-24 21:36:27 -0300

    Adicionando nova branch

---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_01_javascript_primeiros_passos/execicio05.js
@@ -0,0 +1,5 @@
+let tabuada = 9;
+
+for(let i = 1; i < 10; i++){
+    console.log(i + " x " + tabuada + " = " + i * tabuada )
+}
---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_01_javascript_primeiros_passos/exercicio06.js
@@ -0,0 +1,6 @@
+let listaDeNomes = ["Jeferson", "Evellyn","Emilly"];
+
+for(let i = 0; i < listaDeNomes.length;i++){
+    let mensagem = "Boas Vindas " + listaDeNomes[i] + " ! ";
+    console.log(mensagem);
+}
