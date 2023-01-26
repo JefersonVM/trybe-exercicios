22da19d4ce0a8db0a74894ae0ab0e5df4dd711e5
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-01-26 20:53:10 -0300

    Adicionando exercícios

---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_04_javascript_objetos_e_funcoes/exemplos_dia04/exercicio05.js
@@ -0,0 +1,18 @@
+
+//For In
+
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+  }
---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_04_javascript_objetos_e_funcoes/exemplos_dia04/exercicio06.js
@@ -0,0 +1,7 @@
+let food = ['hamburguer', 'bife', 'acarajé'];
+
+for (let position in food) {
+    console.log(position);
+  };
+  //resultado: 0, 1, 2;
+
