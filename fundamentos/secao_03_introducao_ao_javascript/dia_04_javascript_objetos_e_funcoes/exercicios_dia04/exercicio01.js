915c0b1d15352f0787c5fc7a0ad1e407ceefa6ef
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-01-27 04:10:23 -0300

    Finalizando exercícios

---
+++ fundamentos/secao_03_introducao_ao_javascript/dia_04_javascript_objetos_e_funcoes/exercicios_dia04/exercicio01.js
@@ -0,0 +1,40 @@
+let info1 = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+
+//console.log(' Bem-vinda, ' + info.personagem);
+//console.log(info);
+
+
+/* for (let i in info ){
+    console.log(i);
+} */
+
+/* for (let i in info ){
+    console.log(info[i]);
+}
+ */
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  };
+
+  console.log(info1.personagem + ' e ' + info2.personagem + "\n" +
+    info1.origem + ' e ' + info2.origem + "\n" + 
+    info1.nota + ' e ' + info2.nota + "\n" + 
+    'Ambos recorrentes'
+  
+  
+  
+  );
+
+  
+
+
+  
