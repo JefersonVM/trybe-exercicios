a38eef90be9cd732d6f62c3fb19dcd4721dd2e74
Jeferson Vieira <jeferson.vm1981@gmail.com>
2023-02-05 16:53:37 -0300

    Incluindo exercícios

---
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_01_javascript_dom_e_seletores/script.js
@@ -0,0 +1,4 @@
+const myName = 'Jeferson';
+const birthCity = 'São Caetano do Sul';
+let birtyhYear = 1981;
+console.log(myName, birthCity, birtyhYear);
---
+++ fundamentos/secao_04_javascript_dom_eventos_e_web_storage/dia_01_javascript_dom_e_seletores/script02.js
@@ -0,0 +1,4 @@
+const base = 5;
+const height = 8;
+const area = (base * height);
+const perimeter = 5 + 5 + 8 + 8;
